bcaf844dcba61c79f5a031a605df728d6f16c185
nhatvu148 <nhatvu148@gmail.com>
2024-09-24 00:37:10 +0700

    use pairWithTargetSum

--- src/target-sum.ts
+++ src/target-sum.ts
@@ -16,9 +16,23 @@ class Solution {
     }
     return [-1, -1];
   }
+
+  pairWithTargetSum(arr, targetSum) {
+    const nums = {}; // to store numbers and their indices
+    for (let i = 0; i < arr.length; i++) {
+      const num = arr[i];
+      if (targetSum - num in nums) {
+        return [nums[targetSum - num], i];
+      }
+      nums[num] = i;
+    }
+    return [-1, -1];
+  }
 }
 
 const sol = new Solution();
 console.log(sol.search([1, 2, 3, 4, 6], 6));
 console.log(sol.search([2, 5, 9, 11], 11));
 console.log(sol.search([0, 1, 2, 3, 4], 0));
+console.log(sol.pairWithTargetSum([1, 2, 3, 4, 6], 6));
+console.log(sol.pairWithTargetSum([2, 5, 9, 11], 11));
